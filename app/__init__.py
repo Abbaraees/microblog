0618619d6595cb19776f26da78cf9d5f62b3fd10
Muhammad Lawal <abbaraees@gmail.com>
2021-10-28 12:42:18 +0100

    Add Bootstrap Styles
    With this new update our application include styles from the popular
    CSS Framework 'Bootstrap', and has much nicer user interface than the
    basic HTML we are using before.

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
@@ -21,6 +22,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 if not app.debug:
