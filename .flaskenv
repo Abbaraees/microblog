5b989293a576d44b6a7128538edd0d4b0b45d962
Muhammad Lawal <abbaraees@gmail.com>
2021-11-28 12:47:42 +0100

    Handle Dates  and Time

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+export FLASK_APP=microblog.py
+export FLASK_ENV=development
--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from config import Config
 
@@ -23,6 +24,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 if not app.debug:
